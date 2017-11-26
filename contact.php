c8782534762da094807602fd7827a412a92c9d1f
aaraymond <aaraymond@me.com>
2017-11-25 19:05:51 -0500

    Removed old content, added new
    removed old content and added new content. Comments for designer about possible layout patterns and css grid

---
+++ contact.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Shoes</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto+Condensed" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation.css">
+    <link rel="stylesheet" type="text/css" href="css/form.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#Shoes">SHOES</a></li>
+                    <li><a href="#Overview">Overview</a></li>
+                    <li><a href="#Brands">Brands</a></li>
+                    <li><a href="#Styles">Styles</a></li>
+                    <li><a href="#Survey">Survey</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <section id="Shoes">
+            <h1>Do you shoe?</h1>
+            <!-- This section will include a banner or graphic with two buttons, take survey or read more displayed in the terminal area. -->
+        </section>
+
+        <section id="Overview">
+            <h2>About The Site</h2>
+            <p>Welcome to our site! If you would like to read a brief history of shoes, learn the top current brands or most common styles please continue reading. Otherwise, feel free to head down to the <a href="#Survey">Survey</a>.</p>
+
+            <h2>Definition</h2>
+            <p>shoe | SHo͞o | - noun</p>
+            <p>a covering for the foot, typically made of leather, having a sturdy      sole and not reaching above the ankle.</p>
+            <p>Source: New Oxford American Dictionary (Second Edition).</p>
+
+            <h2>History</h2>
+            <p>There is a great deal of evidence to suggest that shoes have been worn by humans for tens of thousands of years but the oldest recovered example found is 10,000 years old (Source: University of Oregon - The Museum of Natural and Cultural History). The world’s oldest leather shoe is a 5,500 year old specimen found in an Armenian cave (Source: National Geographic).</p>
+        </section>
+
+        <section id="Brands">
+            <h2>Popular Brands Today</h2>
+            <!-- Possibly make a horizontal grid layout and use the brand logo images instead of words, then on hover display the brand name. -->
+            <ol>
+                <li>Nike</li>
+                <li>Adidas</li>
+                <li>Jordan (Nike Inc.)</li>
+                <li>Converse (Nike Inc.)</li>
+            </ol>
+
+            <p>Source: Fortune.com</p>
+        </section>
+
+        <section id="Styles">
+            <section>
+                <h2>Men's</h2>
+                <!-- Possibly make a horizontal grid layout and use the style images instead of words, then on hover display the style name. -->
+                <ul>
+                    <li>Dress</li>
+                    <li>Casual</li>
+                    <li>Athletic</li>
+                </ul>
+            </section>
+            <section>
+                <h2>Women's</h2>
+                <!-- Possibly make a horizontal grid layout and use the style images instead of words, then on hover display the style name. -->
+                <ul>
+                    <li>Heels</li>
+                    <li>Flats</li>
+                    <li>Athletic</li>
+                </ul>
+            </section>
+        </section>
+        
+
+        <section id="Survey">
+            <h2>Shoe Survey</h2>
+
+            <form method="post">
+                
+            </form>
+        </section>
+
+        <footer>
+            Information Architecture - Aaron Raymond | Design - Hillary Chun | Front End - Ali Hashim | Back End - John Denzer
+        </footer>
+    </div><!--.container-->
+    
+</body>
+
+</html>
