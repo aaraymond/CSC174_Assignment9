0b3166bf907702df7e7a024fb5b2306fc2eee378
JDDenzer9 <31746765+JDDenzer9@users.noreply.github.com>
2017-12-02 15:10:15 -0500

    added auto redirect to deleteform

--- deleteform.php
+++ deleteform.php
@@ -16,9 +16,20 @@ $sql = "DELETE FROM survey WHERE counter=$counter";
 if(mysqli_query($link, $sql)){
     echo "The records associated with $name were deleted successfully.";
 } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    echo "ERROR: Could not execute $sql. " . mysqli_error($link);
 }
  
 // Close connection
 mysqli_close($link);
 ?>
+
+<html>
+    <body>
+    <p>You will be redirected back to the admin page in 5 seconds!</p>
+    <script>
+        var timer = setTimeout(function() {
+            window.location='http://localhost/CSC174_Assignment9/admin.php'
+        }, 5000);
+    </script>
+</body>
+</html>
